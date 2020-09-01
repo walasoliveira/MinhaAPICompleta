7560adf9ce4b7f5d0b37a388c1fc1439fc73da91
Walas de Oliveira Matos <walasoliveira1@hotmail.com>
2020-09-01 20:03:19 -0300

    Acrescentando configurações

---
+++ src/DevIO.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using MinhaAppMvcCompleta.Models;
+
+namespace DevIO.Api.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolverDependency(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+            return services;
+        }
+    }
+}
